0a68a8331bc98762892d26aed88e0462093b91a9
mac <Mudassar@Qasims-MacBook-Pro.local>
2017-02-15 19:19:15 +0500

    Initial Commit

---
+++ BeautyDoc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BeautyDoc
+//
+//  Created by Mudassar on 15/02/2017.
+//  Copyright © 2017 Mudassar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
